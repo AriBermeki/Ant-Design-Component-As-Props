25f03cd4dad5794de67fb9ca802b7c1b7609880f
Ari Bermeki <90009669+AriBermeki@users.noreply.github.com>
2023-07-02 01:29:10 +0200

    Update Typography.jsx

--- elements/Typography.jsx
+++ elements/Typography.jsx
@@ -1,17 +1,68 @@
 import { Typography } from 'antd';
 
 const UITypography = (props) => {
-  const { type, ...restProps } = props;
+  const { type, code, copyable, delete: del, disabled, editable, ellipsis, keyboard, mark, onClick, strong, italic, underline, level, ...otherProps } = props;
 
   if (type === 'title') {
-    return <Typography.Title {...restProps} />;
+    return (
+      <Typography.Title
+        code={code}
+        copyable={copyable}
+        delete={del}
+        disabled={disabled}
+        editable={editable}
+        ellipsis={ellipsis}
+        level={level}
+        mark={mark}
+        onClick={onClick}
+        italic={italic}
+        type={type}
+        underline={underline}
+        {...otherProps}
+      />
+    );
   } else if (type === 'paragraph') {
-    return <Typography.Paragraph {...restProps} />;
+    return (
+      <Typography.Paragraph
+        code={code}
+        copyable={copyable}
+        delete={del}
+        disabled={disabled}
+        editable={editable}
+        ellipsis={ellipsis}
+        mark={mark}
+        onClick={onClick}
+        strong={strong}
+        italic={italic}
+        type={type}
+        underline={underline}
+        {...otherProps}
+      />
+    );
   } else if (type === 'text') {
-    return <Typography.Text {...restProps} />;
+    return (
+      <Typography.Text
+        code={code}
+        copyable={copyable}
+        delete={del}
+        disabled={disabled}
+        editable={editable}
+        ellipsis={ellipsis}
+        keyboard={keyboard}
+        mark={mark}
+        onClick={onClick}
+        strong={strong}
+        italic={italic}
+        type={type}
+        underline={underline}
+        {...otherProps}
+      />
+    );
   } else {
     return null;
   }
 };
 
 export default UITypography;
+
+
